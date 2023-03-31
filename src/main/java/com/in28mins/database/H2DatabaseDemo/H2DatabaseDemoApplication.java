8fb1d33dfd6aadb3d5d2a55f61c2e7a3e4170abf
Jay <jpca999@gmail.com>
2023-03-31 18:12:48 -0400

    first commit - showing h2 console on / on web

---
+++ src/main/java/com/in28mins/database/H2DatabaseDemo/H2DatabaseDemoApplication.java
@@ -0,0 +1,13 @@
+package com.in28mins.database.H2DatabaseDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2DatabaseDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2DatabaseDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/in28mins/database/H2DatabaseDemo/H2DatabaseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28mins.database.H2DatabaseDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2DatabaseDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
